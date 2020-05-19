de57dedc9f6c30eea4391295c1f1bf567f58e6a4
nariakiiwatani <nariakiiwatani@annolab.com>
2020-05-19 14:49:10 +0900

    部屋作成のフォームを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,44 @@
+import { useState } from "react"
+
 const Index = () => {
-	const handleSubmit = e => {
+
+	const [roomCreation, setRoomCreation] = useState(false)
+
+	const handleCreateSubmit = e => {
 		e.preventDefault()
 
 	}
+	const handleEnterSubmit = e => {
+		e.preventDefault()
+
+	}
+	const createForm = () => (
+		<form onSubmit={handleCreateSubmit}>
+			<span>部屋ID</span><input type="text" name="roomId" /><br />
+			<span>パスワード</span><input type="password" name="password" /><br />
+			<button onClick={handleCreateSubmit}>作成</button>
+		</form>
+	)
 	const enterForm = () => (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleEnterSubmit}>
 			<span>部屋ID</span><input type="text" name="roomId" /><br />
 			<span>パスワード</span><input type="password" name="password" /><br />
 			<span>名前</span><input type="text" name="name" /><br />
 			<span>チーム</span><input type="text" name="team" /><br />
-			<button onClick={handleSubmit}>入室</button>
+			<button onClick={handleEnterSubmit}>入室</button>
 		</form>
+	)
+	const showSwitch = () => (
+		<div>
+			<p onClick={() => { setRoomCreation(false) }}>部屋に入る</p>
+			<p onClick={() => { setRoomCreation(true) }}>部屋を作る</p>
+		</div>
 	)
 	return (
 		<div>
 			<h1>Voext</h1>
-			{enterForm()}
+			{showSwitch()}
+			{roomCreation ? createForm() : enterForm()}
 		</div>
 	);
 };
