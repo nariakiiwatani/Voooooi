f8615382c84464f1bc8f597d93fb50f1640ce164
nariakiiwatani <nariakiiwatani@annolab.com>
2020-05-19 14:38:26 +0900

    入室フォームを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
-const App = () => {
-    return <h1>Hello</h1>
-}
+const Index = () => {
+	const handleSubmit = e => {
+		e.preventDefault()
 
-export default App
+	}
+	const enterForm = () => (
+		<form onSubmit={handleSubmit}>
+			<span>部屋ID</span><input type="text" name="roomId" /><br />
+			<span>パスワード</span><input type="password" name="password" /><br />
+			<span>名前</span><input type="text" name="name" /><br />
+			<span>チーム</span><input type="text" name="team" /><br />
+			<button onClick={handleSubmit}>入室</button>
+		</form>
+	)
+	return (
+		<div>
+			<h1>Voext</h1>
+			{enterForm()}
+		</div>
+	);
+};
+
+export default Index;
