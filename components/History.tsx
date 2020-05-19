ce4f1fa46e31e65d35b55f45ce20b2a8ff14cc17
nariakiiwatani <nariakiiwatani@annolab.com>
2020-05-19 16:30:53 +0900

    Historyコンポーネントを追加

---
+++ components/History.tsx
@@ -0,0 +1,14 @@
+const History = (props) => {
+	const messages = props.messages.slice()
+	messages.sort((a, b) => a.id - b.id)
+
+	return (
+		<div>
+			{messages.map((m, i) => (
+				<div key={i}>{m.text}</div>
+			))}
+		</div>
+	)
+}
+
+export default History
--- pages/rooms/[room].tsx
+++ pages/rooms/[room].tsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
 import VoextInput from '../../components/VoextInput'
+import History from '../../components/History'
 
 const Room = (props) => {
 	const { roomId, username, userteam } = props
+	const [history, setHistory] = useState({
+		lastId: 0,
+		messages: []
+	})
 
 	const debugInfo = () => (
 		<div>
@@ -12,12 +17,19 @@ const Room = (props) => {
 		</div>
 	)
 	const onVoextSubmit = (text) => {
+		const messages = history.messages.slice()
+		messages.push({ id: history.lastId, text })
+		setHistory({
+			lastId: history.lastId + 1,
+			messages
+		})
 	}
 
 	return (
 		<div>
 			{debugInfo()}
 			{<VoextInput onSubmit={onVoextSubmit} />}
+			{<History messages={history.messages} />}
 		</div>
 	)
 }
