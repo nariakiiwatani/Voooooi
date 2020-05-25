b3f80ad0ae19e3a5c525156b2a0fac49f8a2afb6
nariakiiwatani <nariakiiwatani@annolab.com>
2020-05-25 15:12:24 +0900

    コメント背景色設定

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,12 +1,22 @@
 const CommentList = (props) => {
 	const { title, messages, color } = props
 
+	const bgcolor = (color => {
+		const alpha = 0.5;
+		switch (color) {
+			case "red": return `rgba(255,0,0,${alpha})`;
+			case "blue": return `rgba(0,0,255,${alpha})`;
+			case "yellow": return `rgba(255,255,0,${alpha})`;
+			case "white": return `rgba(128,128,128,${alpha})`;
+		}
+	})(color)
+
 	return (
 		<div className="wrapper">
 			<div className="header">{title}</div>
 			<div className="comments">
 				{messages.map((m, i) => (
-					<div key={i}>{m.text}</div>
+					<div key={i}>{m.userName}:{m.text}</div>
 				))}
 				<div className="dummy"></div>
 			</div>
@@ -17,7 +27,7 @@ const CommentList = (props) => {
 				flex-direction: column;
 				height: calc(50vh - 20px);
 				overflow: hidden;
-				background-color: ${color};
+				background-color: ${bgcolor};
 				border: black 1px solid;
 				border-radius: 12px;
 			}
