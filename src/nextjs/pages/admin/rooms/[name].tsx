e31f8fa6ffad1c6309adb77bc40a77418d750b05
nariakiiwatani <nariakiiwatani@annolab.com>
2020-06-04 17:30:29 +0900

    管理ページに部屋のURLを記載

--- src/nextjs/pages/admin/rooms/[name].tsx
+++ src/nextjs/pages/admin/rooms/[name].tsx
@@ -1,11 +1,30 @@
+import { useCollection } from '@nandorojo/swr-firestore'
+import Link from "next/link"
+
 const RoomAdminPage = (props) => {
 	const { roomName, password, pwd } = props
+	const rooms = useCollection("rooms",
+		{
+			where: [
+				["name", "==", roomName],
+				["pwd", "==", pwd]
+			],
+			limit: 1
+		}
+	)
+	const isRoomValid = () => (rooms && rooms.data && rooms.data.length)
+
 	return (
 		<>
 			<div>admin page</div>
-			<div>まだ何もできない</div>
 			<div>部屋名:{roomName}</div>
 			<div>パスワード:{password}</div>
+			<div>
+				<Link href={`/rooms/${roomName}?pwd=${pwd}`}><a>入室リンク(パスワードあり）</a></Link>
+			</div>
+			<div>
+				<Link href={`/admin/rooms/${roomName}?password=${password}&pwd=${pwd}`}><a>管理画面（ここ）</a></Link>
+			</div>
 		</>
 	)
 }
